585f3021ff0b4be58766905f6c9160d20aa61d34
srivatsabc <srivatsabc@gmail.com>
2020-03-04 14:02:23 +0530

    Commit

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 app.get('/hello/:name', function (req, res) {
-  console.log("name: " + req.params.name.toUpperCase());
+  console.log("probe: " + req.params.name.toUpperCase());
   res.status(200).json({'Hello Good Boy': req.params.name.toUpperCase(), 'Date': new Date()});
 })
 
 app.get('/blogs/:name', function (req, res) {
-  console.log("name: " + req.params.name.toUpperCase());
+  console.log("api: " + req.params.name.toUpperCase());
   res.status(200).json({'Hello blogs': req.params.name.toUpperCase(), 'Date': new Date()});
 })
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
    console.log("Example app listening at http on tcp/8081")
 })
